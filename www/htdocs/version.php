f5185a246021ebcc324f2ee81688e8f8761b3889
Roger C Guilherme <rogercguilherme@gmail.com>
2026-06-22 22:41:22 -0300

    Update version.php

--- www/htdocs/version.php
+++ www/htdocs/version.php
@@ -2,5 +2,5 @@
 // This file defines the current version of the software.
 // It is the only place that needs to be changed with each new version.
 
-define('ABCD_VERSION', 'v3.8.3');
-define('DATE_VERSION', '2026-06-16');
+define('ABCD_VERSION', 'v3.8.4');
+define('DATE_VERSION', '2026-06-22');
